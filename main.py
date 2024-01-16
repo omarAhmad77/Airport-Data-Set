23f6cd7d031dd915422daaef0b0daaea57ffd7c1
omarAhmad77 <116854035+omarAhmad77@users.noreply.github.com>
2024-01-16 16:18:30 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#Author: Omar Darwish
+#Assignment Title: Program 27
+#Assignment Description: LAX DataSet
+#Due Date:  11/29/2023
+#Date Created: 11/27/2023
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_name = input("Enter the CSV file name: ")
+df = pd.read_csv(file_name)
+
+average_delays = df['Delayed'].mean()
+average_cancellations = df['Cancelled'].mean()
+print(f"Average delays: {average_delays:.2f}")
+print(f"Average cancellations: {average_cancellations:.2f}")
+
+plt.figure(figsize = (10,6))
+plt.plot(df['Month'], df['Delayed'], label = ['Delays'], marker = 'o')
+plt.plot(df['Month'], df['Cancelled'], label = ['Cancellations'], marker = 'x')
+
+plt.xlabel("Months", fontsize = 10)
+plt.ylabel("Number of flights", fontsize = 10)
+plt.title("Flight status at LAX", fontsize = 14)
+plt.legend()
+
+plt.savefig('output_fig.png')
+plt.show()
